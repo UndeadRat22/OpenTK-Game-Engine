5d16cad0df70069c68dfba9c18d3a42979c5bc16
DeadRat22 - Valdas Rakutis <valdas@rakutis.lt>
2018-02-17 11:11:14 +0200

    Add project files.

---
+++ OpenTK-game-engine/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTK_game_engine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
